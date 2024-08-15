0418a672d2b64a749f1a1ea0498625ce29f88ce5
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-08-15 08:11:43 +0300

    container component

---
+++ src/component/Dialogs/DialogsContainer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import s from './Dialogs.module.css'
+
+import DialogsItem from './../Dialogs/DialogsItem/DialogsItem';
+import Messages from './../Dialogs/Messages/Messages';
+import {sendMessageBodyCreator, updateNewMessageBodyCreator} from "../../redux/dialogs_reducer";
+
+
+const Dialogs = (props) => {
+
+    let state = props.store.getState().dialogsPage
+
+    let dialogsElements = state.dialogs.map(d => <DialogsItem name={d.name} id={d.id}/>)
+    let messagesElements = state.messagesData.map(m => <Messages messages={m.message}/>)
+    let newMessageBody = state.newMessageBody;
+
+    let onSendMessageClick = () => {
+        props.store.dispatch( sendMessageBodyCreator())
+
+    }
+    let onNewMessageChange = (e) => {
+        let body = e.target.value;
+        props.store.dispatch( updateNewMessageBodyCreator(body))
+    }
+
+
+    return <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                {dialogsElements}
+            </div>
+
+
+            <div className={s.messages}>
+                <div>{messagesElements}</div>
+            </div>
+            <div>
+            <div><textarea value={newMessageBody} onChange={onNewMessageChange} placeholder='eee kseniadimkavalerka'></textarea></div>
+                <div><button onClick={onSendMessageClick } className={s.button}>Спросить!</button></div>
+            </div>
+        </div>
+}
+
+export default Dialogs
---
+++ src/component/Profile/MyPosts/Post/MyPostsContainer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import s from './MyPosts.module.css'
+import Post from "./Post/Post"
+import {addPostActionCreator, updateNewPostActionCreator} from "../../../redux/profile_reducer";
+
+
+const MyPosts = (props) => {
+    let pestElement =
+        props.posts.map(p =>  <Post message={p.message} name={p.name} likecount={p.likecount}/>)
+
+    let newPostElement = React.createRef()
+
+    let addPost = () => {
+        // props.addPost()
+        props.dispatch(addPostActionCreator())
+    }
+
+    let onPostChange = () => {
+        let text = newPostElement.current.value;
+        // props.updateNewPostText(text)
+        // props.dispatch({ type: 'UPDATE_NEW_POST_TEXT', newText: text});
+        let action = updateNewPostActionCreator(text)
+        props.dispatch(action)
+    }
+
+
+
+    return <div className={s.postBlosk}>
+           <h3>My post</h3>
+            <div>
+                <div>
+                    <textarea onChange={onPostChange} ref={newPostElement} name="" id="" cols="30" rows="10" value={props.newPostText}/>
+                </div>
+                <div>
+                    <button onClick={ addPost }>Add post</button>
+                </div>
+            </div>
+            <div className={s.post}>
+                {pestElement}
+        </div>
+    </div>
+
+}
+export default MyPosts;
